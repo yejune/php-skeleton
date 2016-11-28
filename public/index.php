c601de46176aff5b19e64a7e7c52a69dba2f42c2
yejune <kwon@yejune.com>
2016-11-28 11:43:26 +0900

    Adding a message when composer.phar is not installed

--- public/index.php
+++ public/index.php
@@ -6,6 +6,13 @@ use \Phalcon\DI\FactoryDefault as Di;
 try {
     define('__BASE__', dirname(dirname(__FILE__)));
 
+    if (false === file_exists(__BASE__.'/vendor/autoload.php')) {
+        die(
+            'You need to set up the project dependencies using the following commands:'.PHP_EOL.
+            'wget http://getcomposer.org/composer.phar'.PHP_EOL.
+            'php composer.phar install'.PHP_EOL
+        );
+    }
     include_once __BASE__.'/vendor/autoload.php';
     include_once __BASE__.'/app/helpers/function.php';
 
